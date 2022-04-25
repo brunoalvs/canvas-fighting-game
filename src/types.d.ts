5facf456ce820059e44b751ced4eeea86c4fdb93
Bruno Alves <hello.brunoalves@gmail.com>
2022-04-25 05:03:51 +0100

    types/index.d.ts renamed to types.d.ts

---
+++ src/types.d.ts
@@ -0,0 +1,45 @@
+interface ISprite {
+  position: IPosition
+  velocity: IVelocity
+  width?: number
+  height?: number
+  color?: string
+  lastKeyPressed: string
+  attackBox?: {
+    position: IPosition
+    width: number
+    height: number
+  }
+  isAttacking?: boolean
+}
+
+interface IPosition {
+  x: number
+  y: number
+}
+
+interface IVelocity {
+  x: number
+  y: number
+}
+
+interface IKeys {
+  ArrowLeft: {
+    pressed: boolean
+  }
+  ArrowRight: {
+    pressed: boolean
+  }
+  ArrowUp: {
+    pressed: boolean
+  }
+  KeyA: {
+    pressed: boolean
+  }
+  KeyD: {
+    pressed: boolean
+  }
+  KeyW: {
+    pressed: boolean
+  }
+}
